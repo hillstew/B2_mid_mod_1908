96f2fb5a5afc379b1a13afa1b2690eab368e0a82
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-19 16:56:14 -0600

    Add test for student_avg_age in instructor_spec

--- spec/models/instructor_spec.rb
+++ spec/models/instructor_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Instructor, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
+
+  describe "methods" do
+    it "should return the average age of its students" do
+      mike = Instructor.create(name: "Mike")
+      mike.students.create(name: "Hillary", cohort: 1908, age: 30)
+      mike.students.create(name: "Mary", cohort: 1908, age: 20)
+      
+      expect(mike.student_avg_age).to eq(25)
+    end
+  end
 end
